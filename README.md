014405feb0befef9dc72c02fe2b34024a064701c
Jerome Boisvert-Chouinard <github@jbchouinard.net>
2025-03-03 14:00:46 -0500

    Add GraalVM native image support
    - Add native-image alias to deps.edn
    - Create native-image build function in build.clj
    - Add reflection configuration for GraalVM
    - Update README with native image build instructions
    - Fix namespace issues in build.clj
    - Configure native executable to output to target/ directory

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A command-line dice roller written in Clojure.
 - Roll dice with any number of sides
 - Parse and evaluate complex dice expressions like `2d6+1d4+3`
 - Accurate probability modeling using dice algebra
+- Normal approximation for efficient large dice rolls
 - Simple command-line interface
 
 ## Prerequisites
@@ -58,10 +59,33 @@ clojure -T:build uber
 
 This will create a standalone JAR file in the `target` directory.
 
+### Building a Native Image
+
+To build a native executable with GraalVM:
+
+```bash
+# Make sure GraalVM and native-image are installed
+# Then build the native image
+clojure -T:build native-image
+```
+
+This will create a native executable called `cljdice` in the `target` directory.
+The native image offers several advantages over the JAR:
+- Faster startup time
+- Lower memory usage
+- No JVM dependency
+
+You can run the native executable directly:
+
+```bash
+./target/cljdice "3d6+4"
+```
 
 ## Project Structure
 
 - `src/cljdice/core.clj`: Main entry point and CLI handling
-- `src/cljdice/dice.clj`: Core dice functionality
+- `src/cljdice/dice.clj`: Core dice functionality and probability calculations
 - `src/cljdice/parser.clj`: Dice expression parser
+- `src/cljdice/random.clj`: Random number generation utilities including uniform and normal distributions
+- `src/cljdice/profile.clj`: Performance profiling tools for development
 - `test/`: Test files
--- build.clj
+++ build.clj
@@ -1,11 +1,15 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [clojure.java.shell :as shell]
+            [clojure.string :as str]))
 
 (def lib 'cljdice/cljdice)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def native-image-name "cljdice")
+(def native-image-path (str "target/" native-image-name))
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -23,3 +27,43 @@
            :main 'cljdice.core})
   (println "Uber jar built successfully:" uber-file)
   uber-file)
+
+(defn native-uber
+  "Build an uber jar for native image compilation (excludes profile namespace)"
+  [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir
+               :include #(not (re-matches #".*profile\.clj$" %))})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir
+                  :ns-compile #{'cljdice.core 'cljdice.dice 'cljdice.parser 'cljdice.random}})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'cljdice.core})
+  (println "Native uber jar built successfully:" uber-file)
+  uber-file)
+
+(defn native-image [_]
+  (let [jar-path (native-uber nil)
+        command ["native-image"
+                 "--no-fallback"
+                 "--initialize-at-build-time"
+                 "-H:+ReportExceptionStackTraces"
+                 "-H:+UnlockExperimentalVMOptions"
+                 "-H:Log=registerResource:"
+                 "-J-Dclojure.spec.skip-macros=true"
+                 "-J-Dclojure.compiler.direct-linking=true"
+                 "-J-Xmx3g"
+                 (str "-H:Name=" native-image-path)
+                 "-jar" jar-path]
+        _ (println "Building native image with command:" (str/join " " command))
+        result (apply shell/sh command)]
+    (if (zero? (:exit result))
+      (println "Native image built successfully:" native-image-path)
+      (do
+        (println "Native image build failed with exit code:" (:exit result))
+        (println "Error:" (:err result))
+        (println "Output:" (:out result))))))
